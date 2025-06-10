5f5759077e6834ccef2168f658b80706ae9da941
palomaceg <p.canalaecheverria@gmail.com>
2025-06-10 14:36:33 +0200

    modificada ruta para coincidir con endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
 const { authentication } = require("../middlewares/authentication");
 const UserController = require("../controllers/userController");
+
 const router = express.Router();
 
 router.post("/", UserController.create);
